2a1a6f8c3293b1dc20741cb9e0422e8f4e9bc000
kevin <kevinander@hotmail.es>
2019-10-28 22:24:25 -0600

    corre el programa

--- src/service/Services/EmailSender.cs
+++ src/service/Services/EmailSender.cs
@@ -1,43 +1,43 @@
 ﻿
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.Extensions.Options;
-using SendGrid;
-using SendGrid.Helpers.Mail;
-using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Identity.UI.Services;
+//using Microsoft.Extensions.Options;
+//using SendGrid;
+//using SendGrid.Helpers.Mail;
+//using System.Threading.Tasks;
 
-namespace Services
-{
-    public class EmailSender : IEmailSender
-    {
-        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
-        {
-            Options = optionsAccessor.Value;
-        }
+//namespace Services
+//{
+//    public class EmailSender : IEmailSender
+//    {
+//        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+//        {
+//            Options = optionsAccessor.Value;
+//        }
 
-        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+//        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
 
-        public Task SendEmailAsync(string email, string subject, string message)
-        {
-            return Execute(Options.SendGridKey, subject, message, email);
-        }
+//        public Task SendEmailAsync(string email, string subject, string message)
+//        {
+//            return Execute(Options.SendGridKey, subject, message, email);
+//        }
 
-        public Task Execute(string apiKey, string subject, string message, string email)
-        {
-            var client = new SendGridClient(apiKey);
-            var msg = new SendGridMessage()
-            {
-                From = new EmailAddress("SIMEPADF@padf.com", "Kevin Perez"),
-                Subject = subject,
-                PlainTextContent = message,
-                HtmlContent = message
-            };
-            msg.AddTo(new EmailAddress(email));
+//        public Task Execute(string apiKey, string subject, string message, string email)
+//        {
+//            var client = new SendGridClient(apiKey);
+//            var msg = new SendGridMessage()
+//            {
+//                From = new EmailAddress("SIMEPADF@padf.com", "Kevin Perez"),
+//                Subject = subject,
+//                PlainTextContent = message,
+//                HtmlContent = message
+//            };
+//            msg.AddTo(new EmailAddress(email));
 
-            // Disable click tracking.
-            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
-            msg.SetClickTracking(false, false);
+//            // Disable click tracking.
+//            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+//            msg.SetClickTracking(false, false);
 
-            return client.SendEmailAsync(msg);
-        }
-    }
-}
+//            return client.SendEmailAsync(msg);
+//        }
+//    }
+//}
